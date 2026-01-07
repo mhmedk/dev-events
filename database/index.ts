7f8dd9dd21c78e5328e4186fbc6bdad52e9c7e7e
Mohammed <mohammed.kasbi45@gmail.com>
2026-01-07 17:10:16 +0100

    feat: implement database models --DONE

---
+++ database/index.ts
@@ -0,0 +1,7 @@
+// Database models exports
+export { default as Booking } from "./booking.model";
+export { default as Event } from "./event.model";
+
+// TypeScript interfaces exports
+export type { IBooking } from "./booking.model";
+export type { IEvent } from "./event.model";
